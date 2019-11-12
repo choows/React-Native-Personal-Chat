3ec1556af99b99628a9f9cefe970b747898fb0c6
Choo Wei Sung <wschoo@INTECH.COM>
2019-11-12 18:41:06 +0800

    Map

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,62 +1,85 @@
 import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 import firebase from 'react-native-firebase';
-import MapView from 'react-native-maps';
+import MapView , {MarkerAnimated, Marker, AnimatedRegion} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import { LOCATION_URL } from '../constants/url';
 import store from '../redux/store';
 import { EventRegister } from 'react-native-event-listeners';
 
 export default class MapScreen extends React.Component {
-    state={
-        self_latitude : 37.78825,
-        self_longitude : -122.4324
+    state = {
+        self_latitude: 37.78825,
+        self_longitude: -122.4324,
+        d_latitude : 0.00,
+        d_longitude : 0.00
     }
-    componentDidMount(){
+    componentDidMount() {
         const state = store.getState();
+        this.GetInitialLocation(state.users.accountId);
         Geolocation.setRNConfiguration({
-            skipPermissionRequests : false,
-            authorizationLevel : 'always'
+            skipPermissionRequests: false,
+            authorizationLevel: 'always'
         });
+        this.StartListenLocation();
+    }
+    GetInitialLocation=(currentUserID)=>{
+        firebase.database().ref(LOCATION_URL).once('value', (snapshot) => {
+            let keys = Object.keys(snapshot.toJSON());
+            keys.map((key)=>{
+                if(key === currentUserID){
+                    this.setState({self_latitude : snapshot.toJSON()[key]["location"]["latitude"]})
+                    this.setState({self_longitude : snapshot.toJSON()[key]["location"]["longititude"]})
+                }else{
+                    this.setState({d_latitude : snapshot.toJSON()[key]["location"]["latitude"]})
+                    this.setState({d_longitude : snapshot.toJSON()[key]["location"]["longititude"]})
+                }
+            })
+        })
+    }
+    StartListenLocation() {
+        firebase.database().ref(LOCATION_URL).on('child_changed', (snapshot) => {
+            if (snapshot.exists()) {
+                console.log(JSON.stringify(snapshot.toJSON()));
+            }
+        })
     }
 
-    SendLocation=()=>{
-        console.log("Here");
+    SendLocation = () => {
         const state = store.getState();
-        
-        Geolocation.getCurrentPosition((position)=>{
-           let location = {
-               latitude : position.coords.latitude,
-               longitude : position.coords.longitude
-           }
+        Geolocation.watchPosition((position) => {
+            let location = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                UID : state.users.accountId
+            }
             firebase.database().ref(LOCATION_URL + state.users.accountId).set({
-                location : location
-            }).catch((err)=>{
+                location: location
+            }).catch((err) => {
                 console.log("Send Message Error : " + err);
             });
-        } , (err)=>{
+        }, (err) => {
             console.log("Error : " + err.message);
-        } , {enableHighAccuracy : true});
+        }, { enableHighAccuracy: true });
     }
 
-    StartListenLocation(){
-       this.SendLocation();
-    }
 
     render() {
         return (
             <View style={styles.container}>
                 <MapView
-                style={styles.MapViewContainer}
+                    style={styles.MapViewContainer}
                     initialRegion={{
                         latitude: this.state.self_latitude,
                         longitude: this.state.self_longitude,
                         latitudeDelta: 0.0922,
                         longitudeDelta: 0.0421,
                     }}
-                />
+                >
+                    <Marker coordinate={ new AnimatedRegion({latitude : this.state.self_latitude , longitude : this.state.self_longitude})} title={"Hi"}/>
+                    </MapView>
                 <TouchableOpacity style={styles.ButtonViewContainer} onPress={this.SendLocation}>
-                    <Text>Start Listen</Text>
+                    <Text style={{ height: '100%', width: '100%', textAlign: 'center' }}>Start Listen</Text>
                 </TouchableOpacity>
             </View>
         )
